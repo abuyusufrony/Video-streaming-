082a355170c080d071138dfa28d3a55d16fbfa45
abuyusufrony <abuyusufrony@gmail.com>
2025-02-13 19:43:10 +0600

    video-dynamic-added

--- script/video.js
+++ script/video.js
@@ -6,7 +6,33 @@ const Loadcatagories = () => {
 
 }
 
+const videoLoad = () => {
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+        .then((res) => res.json())
+        .then((data) => LoadvideoByuser(data.videos))
+        .then((error) => console.log(error))
+}
 
+const LoadvideoByuser = (allVideo) => {
+    const videoConatiner = document.getElementById('video-container')
+    allVideo.forEach((Video) => {
+        const card = document.createElement('div')
+        card.classList = '<div class="card card-compact   ">'
+        card.innerHTML = `<figure>
+    <img
+      src=${Video.thumbnail}
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class=card-title>${Video.title}</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>`
+        videoConatiner.append(card)
+    })
+}
 
 const usercatagorey = (data) => {
     const navCatagory = document.getElementById('catagories');
@@ -22,3 +48,4 @@ const usercatagorey = (data) => {
 }
 
 Loadcatagories()
+videoLoad()
